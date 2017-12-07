3f43ae0e4a7138c75716f940e96868b44502bb21
ZheX <zhex81@gmail.com>
2017-12-08 09:59:17 +1300

    add commander model

---
+++ src/models/Commander.ts
@@ -0,0 +1,33 @@
+import { readFileSync } from 'fs';
+import { Bus } from './Bus';
+import { ICmd } from './interfaces/ICmd';
+
+class Commander {
+    public static load(content: string): Commander {
+        const commander = new Commander();
+        commander.commands = content
+            .split('\n')
+            .filter(c => c !== '')
+            .map(c => commander.parse(c));
+        return commander;
+    }
+
+    public static loadFile(filePath: string): Commander {
+        const content = readFileSync(filePath).toString();
+        return Commander.load(content);
+    }
+
+    public commands: ICmd[];
+
+    public execute(bus: Bus): void {
+        this.commands.forEach(cmd => bus.command(cmd.name, cmd.args));
+    }
+
+    public parse(cmdStr: string): ICmd {
+        const tmp = cmdStr.split(' ');
+        return {
+            name: tmp[0],
+            args: tmp[1] ? tmp[1].split(',') : [],
+        };
+    }
+}
---
+++ src/models/interfaces/ICmd.ts
@@ -0,0 +1,4 @@
+export interface ICmd {
+    name: string;
+    args?: any[];
+}
