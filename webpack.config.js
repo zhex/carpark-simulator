ae78d4566eeb3580571111eb884563627e3024d7
ZheX <zhex81@gmail.com>
2017-12-08 11:47:38 +1300

    setup react env

---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const rootPath = path.join(__dirname, '.');
+const sourcePath = path.join(rootPath, 'src');
+const distPath = path.join(rootPath, 'dist');
+
+module.exports = {
+    entry: './src/main.tsx',
+    output: {
+        path: distPath,
+        filename: '[name].bundle.js',
+    },
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                loader: 'awesome-typescript-loader',
+                include: sourcePath,
+            },
+        ],
+    },
+    resolve: {
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
+        modules: ['node_modules', sourcePath],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'Car Park simulator',
+            filename: 'index.html',
+            template: 'src/index.html',
+        }),
+    ],
+    devServer: {
+        contentBase: distPath,
+    },
+    watchOptions: {
+        ignored: [/node_modules/, distPath],
+    },
+};
