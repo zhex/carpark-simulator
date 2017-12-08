70cc8e6ca9bdce5d9acd4fb290f3b0e22443cd8b
ZheX <zhex81@gmail.com>
2017-12-08 13:18:12 +1300

    add CarkPark component test

---
+++ src/components/__tests__/CarPark.test.tsx
@@ -0,0 +1,24 @@
+import 'jest-styled-components';
+import * as React from 'react';
+import { create } from 'react-test-renderer';
+
+import CarPark, { StyledCell, StyledRow } from '../CarPark';
+
+it('CarPark', () => {
+    const tree = create(<CarPark cols={5} rows={5} cellSize={50} />).toJSON();
+    expect(tree).toMatchSnapshot();
+    expect(tree).toHaveStyleRule('width', '250px');
+});
+
+it('Cell', () => {
+    const tree = create(<StyledCell size={50} />).toJSON();
+    expect(tree).toMatchSnapshot();
+    expect(tree).toHaveStyleRule('height', '50px');
+    expect(tree).toHaveStyleRule('flex', '1');
+});
+
+it('Row', () => {
+    const tree = create(<StyledRow />).toJSON();
+    expect(tree).toMatchSnapshot();
+    expect(tree).toHaveStyleRule('display', 'flex');
+})
